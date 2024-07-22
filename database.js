661c169b9a0239d2ca7775b5be50b6439796ee00
nicogalvang <nico.galvangarcia@gmail.com>
2024-07-22 12:15:52 +0200

    Cambio método conexión BD

--- database.js
+++ database.js
@@ -1,21 +1,20 @@
-var mysql = require('mysql');
+let mysql = require('mysql');
 
-var con = mysql.createConnection({
+let conexion = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "12345",
   database: "mysqlNotas"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Conectado a la base de datos");
-
-  con.query("SELECT * FROM notas", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
+conexion.connect(function(err){
+  if(err){
+    throw err;
+  }else{
+    console.log("conexion exitosa");
+  }
 });
 
+conexion.end();
 
 
