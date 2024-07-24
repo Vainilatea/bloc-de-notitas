9b1fee85aa45b019c1bb2cbcc9346d9af5a22678
nicogalvang <nico.galvangarcia@gmail.com>
2024-07-24 08:39:09 +0200

    Login

---
+++ regUsuario.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const conexion = require ("../config/conexion");
+
+Router.post("/regUsuario", function(req, res)){
+
+    let ide = req.body.campoIde;
+    let ide = req.body.campoIde;
+    let ide = req.body.campoIde;
+    let ide = req.body.campoIde;
+
+}
