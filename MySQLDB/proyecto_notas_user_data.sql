22cd3beb02ae9538374bdb15cb039ad32e2ef467
nicogalvang <nico.galvangarcia@gmail.com>
2024-07-24 10:24:05 +0200

    SQL lista

--- MySQLDB/proyecto_notas_user_data.sql
+++ MySQLDB/proyecto_notas_user_data.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `proyecto_notas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `proyecto_notas`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: proyecto_notas
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-24  9:29:50
+-- Dump completed on 2024-07-24 10:20:14
